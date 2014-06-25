df7fd2d3b257395c963788a17b890ffea29d3c11
Shayan Yousefizadeh <yousefs@tcd.ie>
2014-06-25 10:18:49 +0100

    Mini mini minor changes

--- ExampleProject/Example/TSDemoViewController.m
+++ ExampleProject/Example/TSDemoViewController.m
@@ -198,7 +198,7 @@
 
 - (IBAction)didTapText:(id)sender
 {
-    [TSMessage displayMessageWithTitle:NSLocalizedString(@"With 'Text' I meant a long text, so here it is", nil)
+    [TSMessage displayMessageWithTitle:NSLocalizedString(@"Really long subtitle...", nil)
                               subtitle:NSLocalizedString(@"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus", nil)
                                   type:TSMessageTypeWarning];
 }
